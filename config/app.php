66fbc1f5d3c0b3fc73a294b1ce1946fc720d4922
SuperDev <63515814+Supernova3339@users.noreply.github.com>
2023-03-24 08:45:06 -0500

    Update app.php
    Signed-off-by: SuperDev <63515814+Supernova3339@users.noreply.github.com>

--- config/app.php
+++ config/app.php
@@ -57,6 +57,8 @@ return [
     'url' => env('APP_URL', 'http://localhost'),
 
     'asset_url' => env('ASSET_URL', '/'),
+    
+    'force_https' => env('FORCE_HTTPS', true),
 
     /*
     |--------------------------------------------------------------------------
