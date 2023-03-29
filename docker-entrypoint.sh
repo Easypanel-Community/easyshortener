e507fa62016b804123ca0ed8f27db95d61bc6df5
SuperDev <63515814+Supernova3339@users.noreply.github.com>
2023-03-29 18:55:34 -0500

    Update docker-entrypoint.sh
    Signed-off-by: SuperDev <63515814+Supernova3339@users.noreply.github.com>

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,14 +1,16 @@
 #!/bin/sh
 
-
 composer install --optimize-autoloader --no-dev
 cp /app/vendor /var/www/html/vendor -R
 cp /app/bootstrap/cache /var/www/html/bootstrap/cache -R
-
 if [ ! -f /var/www/html/database/sqlite/easyshortener.db ]; then
     touch /var/www/html/database/sqlite/easyshortener.db
 fi
 chown -R www-data: /var/www/html/database/sqlite
+
+cd /var/www/html
+php artisan key:generate
+
 
 echo "Starting Migrations..."
 php artisan migrate --force --no-interaction
@@ -16,7 +18,6 @@ php artisan migrate --force --no-interaction
 echo "Clearing caches..."
 php artisan config:cache --no-interaction
 php artisan view:cache --no-interaction
-
 chmod -R 777 storage/logs/laravel.log
 
 php-fpm -D && nginx -g 'daemon off;'
