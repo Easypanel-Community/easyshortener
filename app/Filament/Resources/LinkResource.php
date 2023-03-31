985c2a329fa8ed2b5a0c981f29ffe1ea1f629c53
SuperDev <63515814+Supernova3339@users.noreply.github.com>
2023-03-30 20:27:17 -0500

    Update LinkResource.php
    Signed-off-by: SuperDev <63515814+Supernova3339@users.noreply.github.com>

--- app/Filament/Resources/LinkResource.php
+++ app/Filament/Resources/LinkResource.php
@@ -10,6 +10,7 @@ use Filament\Resources\Form;
 use Filament\Resources\Resource;
 use Filament\Resources\Table;
 use Filament\Tables;
+use Filament\Tables\Filters\TernaryFilter;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
 
