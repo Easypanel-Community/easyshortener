2fa2ef9e1b4a6dfe16f4081a3f2e831cc85b23f4
SuperDev <63515814+Supernova3339@users.noreply.github.com>
2023-03-30 21:24:19 -0500

    Update UserResource.php
    Signed-off-by: SuperDev <63515814+Supernova3339@users.noreply.github.com>

--- app/Filament/Resources/UserResource.php
+++ app/Filament/Resources/UserResource.php
@@ -29,10 +29,12 @@ class UserResource extends Resource
                     ->maxLength(1),
                 Forms\Components\TextInput::make('name')
                     ->required()
+                    ->searchable()
                     ->maxLength(255),
                 Forms\Components\TextInput::make('email')
                     ->email()
                     ->required()
+                    ->searchable()
                     ->maxLength(255),
                 Forms\Components\DateTimePicker::make('email_verified_at'),
                 Forms\Components\TextInput::make('password')
