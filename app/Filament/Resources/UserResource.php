f59165b4d1c80c09e870608e7026013396654a20
SuperDev <63515814+Supernova3339@users.noreply.github.com>
2023-03-30 21:35:15 -0500

    Update UserResource.php
    Signed-off-by: SuperDev <63515814+Supernova3339@users.noreply.github.com>

--- app/Filament/Resources/UserResource.php
+++ app/Filament/Resources/UserResource.php
@@ -35,7 +35,7 @@ class UserResource extends Resource
                     ->email()
                     ->required()
                     ->maxLength(255),
-                Forms\Components\DateTimePicker::make('email_verified_at')->default(now()),
+                Forms\Components\DateTimePicker::make('email_verified_at')->toggleable(isToggledHiddenByDefault: true)->default(now()),
                 Forms\Components\TextInput::make('password')
                     ->password()
                     ->required()
