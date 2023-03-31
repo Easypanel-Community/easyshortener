086709c0ba980edd56659e2a0789ef48debffdcf
SuperDev <63515814+Supernova3339@users.noreply.github.com>
2023-03-30 20:01:36 -0500

    Update link-table.blade.php
    Signed-off-by: SuperDev <63515814+Supernova3339@users.noreply.github.com>

--- resources/views/livewire/link-table.blade.php
+++ resources/views/livewire/link-table.blade.php
@@ -15,9 +15,11 @@
                                 <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
                                     Status
                                 </th>
+                                @if(config('easyshortener.enable_analytics') == "true")
                                 <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
                                     Redirects
                                 </th>
+                                @endif
                                 <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
                                     Created
                                 </th>
