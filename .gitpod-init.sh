8963d421e7ae7de9410d2e669cb9202cdb6eb5a8
SuperDev <63515814+Supernova3339@users.noreply.github.com>
2023-04-10 17:10:39 -0500

    Update .gitpod-init.sh
    Signed-off-by: SuperDev <63515814+Supernova3339@users.noreply.github.com>

--- .gitpod-init.sh
+++ .gitpod-init.sh
@@ -2,6 +2,7 @@ mysql -u root -e "create database laravel"
 cp .env.example .env
 sed -i "s|APP_URL=|APP_URL=${GITPOD_WORKSPACE_URL}|g" .env
 sed -i "s|ASSET_URL=|ASSET_URL=${GITPOD_WORKSPACE_URL}|g" .env
+sed -i "s#FORCE_HTTPS=false#FORCE_HTTPS=true" .env
 composer install
 npm i
 npm run build
